07f721e8128e31d5da72d31ce8f5e522859581a1
Teriuihi <teriuihi@alttd.com>
2022-01-13 21:26:01 +0100

    Enable database and load particles

--- src/main/java/com/alttd/AltitudeParticles.java
+++ src/main/java/com/alttd/AltitudeParticles.java
@@ -3,8 +3,10 @@ package com.alttd;
 import com.alttd.commands.CommandManager;
 import com.alttd.config.Config;
 import com.alttd.config.DatabaseConfig;
+import com.alttd.database.Database;
 import com.alttd.listeners.*;
 import com.alttd.objects.APartType;
+import com.alttd.particles.InitParticles;
 import com.alttd.util.Logger;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,9 +28,10 @@ public class AltitudeParticles extends JavaPlugin {
     public void onEnable() {
         Config.reload();
         DatabaseConfig.reload();
-//        Database.getDatabase().init();
+        Database.getDatabase().init();
         new CommandManager();
         registerEvents();
+        InitParticles.init();
         Logger.info("--------------------------------------------------");
         Logger.info("Altitude Particles started");
         Logger.info("--------------------------------------------------");
