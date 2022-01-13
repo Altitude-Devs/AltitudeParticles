5be8722f532227bb5cdb62acd2b2930727c9451b
Teriuihi <teriuihi@alttd.com>
2022-01-13 21:26:16 +0100

    Updated APartType to include db friendly id

--- src/main/java/com/alttd/objects/APartType.java
+++ src/main/java/com/alttd/objects/APartType.java
@@ -4,20 +4,25 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public enum APartType { //TODO add description?
-    HEAD("Head", Material.PLAYER_HEAD, null),
-    TRAIL("Trail", Material.GOLD_INGOT, null),
-    BREAK_PLACE_BLOCK("Break/place block", Material.GRASS_BLOCK, null),
-    DEATH("Death", Material.SKELETON_SKULL, null),
-    ATTACK_CLOSE("Attack CQC", Material.DIAMOND_SWORD, null),
-    ATTACK_RANGE("Attack ranged", Material.BOW, null),
-    TELEPORT_ARRIVE("Teleport", Material.DRAGON_EGG, null);
+    HEAD("HEAD", "Head", Material.PLAYER_HEAD, null),
+    TRAIL("TRAIL", "Trail", Material.GOLD_INGOT, null),
+    BREAK_PLACE_BLOCK("BREAK_PLACE_BLOCK", "Break/place block", Material.GRASS_BLOCK, null),
+    DEATH("DEATH", "Death", Material.SKELETON_SKULL, null),
+    ATTACK_CLOSE("ATTACK_CQC", "Attack CQC", Material.DIAMOND_SWORD, null),
+    ATTACK_RANGE("ATTACK_RANGED", "Attack ranged", Material.BOW, null),
+    TELEPORT_ARRIVE("TELEPORT", "Teleport", Material.DRAGON_EGG, null);
 
     private final String name;
+    private final String displayName;
     private final Material material;
     private ItemStack itemStack;
 
     public String getName() {
         return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public Material getMaterial() {
@@ -32,8 +37,9 @@ public enum APartType { //TODO add description?
         this.itemStack = itemStack;
     }
 
-    APartType(String name, Material material, ItemStack itemStack) {
+    APartType(String name, String displayName, Material material, ItemStack itemStack) {
         this.name = name;
+        this.displayName = displayName;
         this.material = material;
         this.itemStack = itemStack;
     }
