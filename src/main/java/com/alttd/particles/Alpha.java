a8a7a3e9e1087cd77b6d23c294761fcd75696671
Teriuihi <teriuihi@alttd.com>
2022-06-05 17:34:40 +0200

    Fully disabled build in particles

--- src/main/java/com/alttd/particles/Alpha.java
+++ src/main/java/com/alttd/particles/Alpha.java
@@ -17,37 +17,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Alpha {
-    private static final ItemStack itemStack;
-
-    static {
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-        itemStack = new ItemStack(Material.ANVIL);
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.displayName(miniMessage.deserialize("<gold>Alpha Particles</gold>"));
-        itemMeta.lore(List.of(
-                miniMessage.deserialize("<dark_aqua>An exclusive particle</dark_aqua>"),
-                miniMessage.deserialize("<dark_aqua>set for Alpha testers!</dark_aqua>")));
-        itemStack.setItemMeta(itemMeta);
-    }
-
-    public Alpha() {
-        List<Frame> frameList = new ArrayList<>();
-
-        frameList.add(new Frame(frameOne()));
-
-        ParticleStorage.addParticleSet(APartType.TELEPORT_ARRIVE, new ParticleSet(frameList, 5, 10, 10, APartType.TELEPORT_ARRIVE, "ALPHA_TELEPORT", "apart.particle.alpha", itemStack));
-        ParticleStorage.addParticleSet(APartType.CLICK_BLOCK, new ParticleSet(frameList, 5, 10, 10, APartType.CLICK_BLOCK, "ALPHA_CLICK", "apart.particle.alpha", itemStack));
-    }
-
-
-    public List<AParticle> frameOne() {
-        List<AParticle> list = new ArrayList<>();
-        double[] xPts = {0.23, 0.21, 0.19, 0.17, 0.15, 0.13, 0.11, 0.09, 0.06, 0.02, -0.03, -0.08, -0.14, -0.19, -0.24, -0.26, -0.28, -0.28, -0.27, -0.25, -0.22, -0.18, -0.13, -0.08, -0.03, 0.00, 0.03, 0.06, 0.09, 0.11, 0.14, 0.15, 0.16, 0.18, 0.22, 0.26, 0.30, 0.32};
-        double[] yPts = {0.91, 0.86, 0.81, 0.76, 0.70, 0.63, 0.58, 0.52, 0.47, 0.42, 0.39, 0.38, 0.39, 0.42, 0.47, 0.52, 0.58, 0.63, 0.69, 0.75, 0.81, 0.86, 0.89, 0.90, 0.89, 0.87, 0.83, 0.79, 0.74, 0.69, 0.57, 0.51, 0.46, 0.42, 0.40, 0.40, 0.42, 0.47};
-
-        for(int i = 0; i < xPts.length; i++) {
-            list.add(new AParticle(xPts[i] * 2, (yPts[i] * 2) + 1.5, 0.5, 0, new ParticleBuilder(Particle.REDSTONE).color(Color.GRAY).count(1)));
-        }
-        return list;
-    }
+//    private static final ItemStack itemStack;
+//
+//    static {
+//        MiniMessage miniMessage = MiniMessage.miniMessage();
+//        itemStack = new ItemStack(Material.ANVIL);
+//        ItemMeta itemMeta = itemStack.getItemMeta();
+//        itemMeta.displayName(miniMessage.deserialize("<gold>Alpha Particles</gold>"));
+//        itemMeta.lore(List.of(
+//                miniMessage.deserialize("<dark_aqua>An exclusive particle</dark_aqua>"),
+//                miniMessage.deserialize("<dark_aqua>set for Alpha testers!</dark_aqua>")));
+//        itemStack.setItemMeta(itemMeta);
+//    }
+//
+//    public Alpha() {
+//        List<Frame> frameList = new ArrayList<>();
+//
+//        frameList.add(new Frame(frameOne()));
+//
+//        ParticleStorage.addParticleSet(APartType.TELEPORT_ARRIVE, new ParticleSet(frameList, 5, 10, 10, APartType.TELEPORT_ARRIVE, "ALPHA_TELEPORT", "apart.particle.alpha", itemStack));
+//        ParticleStorage.addParticleSet(APartType.CLICK_BLOCK, new ParticleSet(frameList, 5, 10, 10, APartType.CLICK_BLOCK, "ALPHA_CLICK", "apart.particle.alpha", itemStack));
+//    }
+//
+//
+//    public List<AParticle> frameOne() {
+//        List<AParticle> list = new ArrayList<>();
+//        double[] xPts = {0.23, 0.21, 0.19, 0.17, 0.15, 0.13, 0.11, 0.09, 0.06, 0.02, -0.03, -0.08, -0.14, -0.19, -0.24, -0.26, -0.28, -0.28, -0.27, -0.25, -0.22, -0.18, -0.13, -0.08, -0.03, 0.00, 0.03, 0.06, 0.09, 0.11, 0.14, 0.15, 0.16, 0.18, 0.22, 0.26, 0.30, 0.32};
+//        double[] yPts = {0.91, 0.86, 0.81, 0.76, 0.70, 0.63, 0.58, 0.52, 0.47, 0.42, 0.39, 0.38, 0.39, 0.42, 0.47, 0.52, 0.58, 0.63, 0.69, 0.75, 0.81, 0.86, 0.89, 0.90, 0.89, 0.87, 0.83, 0.79, 0.74, 0.69, 0.57, 0.51, 0.46, 0.42, 0.40, 0.40, 0.42, 0.47};
+//
+//        for(int i = 0; i < xPts.length; i++) {
+//            list.add(new AParticle(xPts[i] * 2, (yPts[i] * 2) + 1.5, 0.5, 0, new ParticleBuilder(Particle.REDSTONE).color(Color.GRAY).count(1)));
+//        }
+//        return list;
+//    }
 }
--- src/main/java/com/alttd/particles/Test.java
+++ src/main/java/com/alttd/particles/Test.java
@@ -16,41 +16,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-
-    private static final ItemStack itemStack;
-
-    static {
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-        itemStack = new ItemStack(Material.BUCKET);
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.displayName(miniMessage.deserialize("<gold>TestParticles</gold>"));
-        itemMeta.lore(List.of(
-                miniMessage.deserialize("<dark_aqua>A particle to test</dark_aqua>"),
-                miniMessage.deserialize("<dark_aqua>the functionality of this plugin</dark_aqua>")));
-        itemStack.setItemMeta(itemMeta);
-    }
-
-    public Test() {
-        APartType test = APartType.BREAK_PLACE_BLOCK;
-        List<Frame> frameList = new ArrayList<>();
-        //
-        frameList.add(new Frame(frameOne()));
-        //....
-        ParticleStorage.addParticleSet(test, new ParticleSet(frameList, 5, 5, 0, test, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
-        ParticleStorage.addParticleSet(APartType.TRAIL, new ParticleSet(frameList, 5, -1, 40, APartType.TRAIL, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
-        ParticleStorage.addParticleSet(APartType.DEATH, new ParticleSet(frameList, 5, 2, 10, APartType.DEATH, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
-        ParticleStorage.addParticleSet(APartType.KILL, new ParticleSet(frameList, 5, -1, 10, APartType.KILL, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
-        ParticleStorage.addParticleSet(APartType.TELEPORT_ARRIVE, new ParticleSet(frameList, 5, 5, 40, APartType.TELEPORT_ARRIVE, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
-        ParticleStorage.addParticleSet(APartType.CLICK_BLOCK, new ParticleSet(frameList, 5, 5, 40, APartType.CLICK_BLOCK, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
-    }
-
-
-    public List<AParticle> frameOne() {
-        List<AParticle> list = new ArrayList<>();
-        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
-        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
-        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
-        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
-        return list;
-    }
+//
+//    private static final ItemStack itemStack;
+//
+//    static {
+//        MiniMessage miniMessage = MiniMessage.miniMessage();
+//        itemStack = new ItemStack(Material.BUCKET);
+//        ItemMeta itemMeta = itemStack.getItemMeta();
+//        itemMeta.displayName(miniMessage.deserialize("<gold>TestParticles</gold>"));
+//        itemMeta.lore(List.of(
+//                miniMessage.deserialize("<dark_aqua>A particle to test</dark_aqua>"),
+//                miniMessage.deserialize("<dark_aqua>the functionality of this plugin</dark_aqua>")));
+//        itemStack.setItemMeta(itemMeta);
+//    }
+//
+//    public Test() {
+//        APartType test = APartType.BREAK_PLACE_BLOCK;
+//        List<Frame> frameList = new ArrayList<>();
+//        //
+//        frameList.add(new Frame(frameOne()));
+//        //....
+//        ParticleStorage.addParticleSet(test, new ParticleSet(frameList, 5, 5, 0, test, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
+//        ParticleStorage.addParticleSet(APartType.TRAIL, new ParticleSet(frameList, 5, -1, 40, APartType.TRAIL, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
+//        ParticleStorage.addParticleSet(APartType.DEATH, new ParticleSet(frameList, 5, 2, 10, APartType.DEATH, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
+//        ParticleStorage.addParticleSet(APartType.KILL, new ParticleSet(frameList, 5, -1, 10, APartType.KILL, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
+//        ParticleStorage.addParticleSet(APartType.TELEPORT_ARRIVE, new ParticleSet(frameList, 5, 5, 40, APartType.TELEPORT_ARRIVE, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
+//        ParticleStorage.addParticleSet(APartType.CLICK_BLOCK, new ParticleSet(frameList, 5, 5, 40, APartType.CLICK_BLOCK, "UNIQUE_NAME_TEST", "apart.particle.test", itemStack));
+//    }
+//
+//
+//    public List<AParticle> frameOne() {
+//        List<AParticle> list = new ArrayList<>();
+//        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
+//        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
+//        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
+//        list.add(new AParticle(0, 2, 0, 0.3, new ParticleBuilder(Particle.TOTEM)));
+//        return list;
+//    }
 }
